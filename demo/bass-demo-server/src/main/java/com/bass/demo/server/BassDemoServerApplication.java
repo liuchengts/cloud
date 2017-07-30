2607b9d580c9c02718daa89749a7c5ce5a974b3b
liuchengts <790809568@qq.com>
2017-07-30 16:47:35 +0800

    dubbo整合

--- demo/bass-demo-server/src/main/java/com/bass/demo/server/BassDemoServerApplication.java
+++ demo/bass-demo-server/src/main/java/com/bass/demo/server/BassDemoServerApplication.java
@@ -3,8 +3,11 @@ package com.bass.demo.server;
 
 import com.bass.support.BassSupportApplication;
 import org.mvnsearch.spring.boot.dubbo.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
 
 @EnableDubboConfiguration("com.bass.demo.server.dubbo")
 public class BassDemoServerApplication extends BassSupportApplication{
-
+    public static void main(String[] args) {
+        SpringApplication.run(BassDemoServerApplication.class, args);
+    }
 }
---
+++ demo/bass-demo-server/src/test/java/com/bass/demo/server/service/impl/ApiOperationServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.bass.demo.server.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by apple on 2017/7/30.
+ */
+public class ApiOperationServiceImplTest {
+
+    @Test
+    public void fandById() throws Exception {
+    }
+
+}
