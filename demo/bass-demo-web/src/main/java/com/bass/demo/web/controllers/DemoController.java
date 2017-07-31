e2e320ef729fa4fe4225fcaebf33cc129c223ac5
liuchengts <790809568@qq.com>
2017-07-31 12:06:01 +0800

    集成了dubbo，springcloud，jpa

--- demo/bass-demo-web/src/main/java/com/bass/demo/web/controllers/DemoController.java
+++ demo/bass-demo-web/src/main/java/com/bass/demo/web/controllers/DemoController.java
@@ -1,30 +1,30 @@
-//package com.bass.demo.web.controllers;
-//
-//import com.bass.demo.common.dubbo.ApiOperationManager;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-///**
-// * Created by apple on 2017/7/21.
-// * lc
-// * web服务 为页面提供服务
-// */
-//@Controller
-//@RequestMapping("/api")
-//public class DemoController {
-//    private final ApiOperationManager apiOperationManager;
-//
-//    @Autowired
-//    public DemoController(ApiOperationManager apiOperationManager) {
-//        this.apiOperationManager = apiOperationManager;
-//    }
-//
-//    @RequestMapping("/fandById/{id}")
-//    public String fandById(@PathVariable("id") String id, ModelMap model) {
-//        model.put("couponBatch", apiOperationManager.fandById(id));
-//        return "api/index";
-//    }
-//}
+package com.bass.demo.web.controllers;
+
+import com.bass.demo.common.dubbo.ApiOperationManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by apple on 2017/7/21.
+ * lc
+ * web服务 为页面提供服务
+ */
+@Controller
+@RequestMapping("/api")
+public class DemoController {
+    private final ApiOperationManager apiOperationManager;
+
+    @Autowired
+    public DemoController(ApiOperationManager apiOperationManager) {
+        this.apiOperationManager = apiOperationManager;
+    }
+
+    @RequestMapping("/fandById/{id}")
+    public String fandById(@PathVariable("id") String id, ModelMap model) {
+        model.put("couponBatch", apiOperationManager.fandById(id));
+        return "api/index";
+    }
+}
