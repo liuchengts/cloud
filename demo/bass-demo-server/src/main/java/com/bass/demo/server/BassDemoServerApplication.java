3676c10654e4b44a1697417962d9991ae076a591
liuchengts <790809568@qq.com>
2017-08-03 18:55:18 +0800

    添加说明

--- demo/bass-demo-server/src/main/java/com/bass/demo/server/BassDemoServerApplication.java
+++ demo/bass-demo-server/src/main/java/com/bass/demo/server/BassDemoServerApplication.java
@@ -5,7 +5,6 @@ import com.bass.support.BassSupportApplication;
 import org.mvnsearch.spring.boot.dubbo.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableDubboConfiguration("com.bass.demo.server.dubbo")
 @EntityScan({"com.bass.demo.common.model"})
